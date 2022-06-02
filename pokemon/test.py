386ab8949131c2f869db12e78b8e3d4fa360919e
1903012 <1903012@narlabs.org.tw>
2022-06-02 14:34:26 +0800

    test

--- pokemon/test.py
+++ pokemon/test.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import pandas as pd
 res = requests.get('https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_evolution_family')
 # print(res.text)
 
@@ -9,23 +9,25 @@ soup = BeautifulSoup(res.text, "html.parser")
 
 # print(soup.prettify())
 soup_tables = soup.select('table.roundy tbody')
+families = []
+
 for soup_table in soup_tables:
 
     tr_list = soup_table.select('tr')
-    print(len(tr_list))
 
     for tr in tr_list:
         td_list = tr.select('td')
-        print("---")
+        # print("---")
+        family = []
         for td in td_list:
             a = td.find_all('a', title=re.compile("\(Pokémon\)"))
             if len(a) > 0:
                 pokemon = a[0].select("span")[0].text
-                print(pokemon)
+                # print(pokemon)
+                family.append(pokemon)
+        if len(family) > 0:
+            families.append(family)
 
-            # if _t == "":
-            #     a_list = td.select('a')
-            #     for a in a_list:
-            #         print(a["title"])
-    breakpoint()
 
+print(families)
+print(len(families))
