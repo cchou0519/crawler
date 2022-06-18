815227ff63cfbc14e6558072b52b04995b66d555
cchou0519 <73981474+cchou0519@users.noreply.github.com>
2022-06-19 02:13:34 +0800

    Update create-deployment-yaml-v2.py

--- deployment/create-deployment-yaml-v2.py
+++ deployment/create-deployment-yaml-v2.py
@@ -1,18 +1,19 @@
 
-
 place_name = "HC"
-zoom_out_times = 3
-is_100 = False
+zoom_out_times = 4
+is_100 = True
 is_pvp = True
 
 with open("deployment-template.yaml") as f:
     contents = f.read()
     contents = contents.replace("<place_name>", place_name)\
-        .replace("<zoom_out_times>", str(zoom_out_times))\
-        .replace("<is_100>", str(is_100)).replace("<is_pvp>", str(is_pvp))
+        .replace("<place_name_lower>", place_name.lower())\
+        .replace("<zoom_out_times>", '"' + str(zoom_out_times) + '"')\
+        .replace("<is_100>", '"' + str(is_100) + '"').replace("<is_pvp>", '"' + str(is_pvp) + '"')
 
 
-with open("deployment-" + place_name + ".yaml", mode="w") as f:
+with open("deployment-" + place_name.lower() + ".yaml", mode="w") as f:
     f.write(contents)
+
 
 
