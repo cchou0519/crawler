66a4862de03cdf2ad9b29ca7a45ca7ce517960b4
cchou0519 <73981474+cchou0519@users.noreply.github.com>
2022-06-20 10:37:54 +0800

    Update create-deployment-yaml-v2.py

--- deployment/create-deployment-yaml-v2.py
+++ deployment/create-deployment-yaml-v2.py
@@ -1,19 +1,50 @@
+import argparse
 
-place_name = "HC"
-zoom_out_times = 4
-is_100 = True
-is_pvp = True
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--place_name", help="input where, such as HC, TP, TC ...",
+                    type=str)
+parser.add_argument("--zoom_out_times", type=int)
+parser.add_argument("--is_pvp", type=str2bool)
+parser.add_argument("--is_100", type=str2bool)
+
+args = parser.parse_args()
+
+place_name = args.place_name
+zoom_out_times = args.zoom_out_times
+is_100 = args.is_100
+is_pvp = args.is_pvp
+
+if is_100:
+    tag_100 = "-100"
+else:
+    tag_100 = ""
+
+if is_pvp:
+    tag_pvp = "-pvp"
+else:
+    tag_pvp = ""
 
 with open("deployment-template.yaml") as f:
     contents = f.read()
     contents = contents.replace("<place_name>", place_name)\
         .replace("<place_name_lower>", place_name.lower())\
         .replace("<zoom_out_times>", '"' + str(zoom_out_times) + '"')\
-        .replace("<is_100>", '"' + str(is_100) + '"').replace("<is_pvp>", '"' + str(is_pvp) + '"')
+        .replace("<is_100>", '"' + str(is_100) + '"').replace("<is_pvp>", '"' + str(is_pvp) + '"')\
+        .replace("<100_tag>", tag_100).replace("<pvp_tag>", tag_pvp)
 
 
-with open("deployment-" + place_name.lower() + ".yaml", mode="w") as f:
+with open("deployment-" + place_name.lower() + tag_100 + tag_pvp + ".yaml", mode="w") as f:
     f.write(contents)
-
-
-
+    print("create", "deployment-" + place_name.lower() + tag_100 + tag_pvp + ".yaml", "successful")
